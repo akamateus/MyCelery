87e80cc7f654a3d3c7656cd5c546ce6e4c27d172
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-07-21 10:25:51 +0200

    added hooks for theme and navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  ImputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  PersonSearchIcon,
+  TextsmsIcon,
+  EmojiObjectsTwoToneIcon,
+  EmojiObjectsRoundedIcon,
+  NotificationsIcon,
+  InfoIcon,
+  WidgetsIcon,
+  CloseIcon,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBoxBetween from "components/FlexBoxBetween";
+
 const Navbar = () => {
-  return <div>Navbar📑</div>;
+  const [isMobileMenuToggled, setIsmobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonmobileScreens = useMediaQuery("(min-width:1000px)");
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
