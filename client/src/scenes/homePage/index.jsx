c822cdf633230bea9bb8d1eb44678757e8316a67
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-07-26 13:22:53 +0200

    added profile widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "scenes/navbar";
 import { useSelector } from "react-redux";
 import UserInfo from "scenes/widgets/UserInfo";
+import MyPostWidget from "scenes/widgets/PostWidget";
 
 const HomePage = () => {
   const isNotMobileScreen = useMediaQuery("min-width:1000px");
@@ -16,13 +17,15 @@ const HomePage = () => {
         gap="0.5rem"
         justifyContent="space-between"
       >
-        <Box flexBasis={isNotMobileScreen ? "25%" : undefined}>
+        <Box flexBasis={isNotMobileScreen ? "26%" : undefined}>
           <UserInfo userId={_id} picturePath={picturePath} />
         </Box>
         <Box
-          flexBasis={isNotMobileScreen ? "40%" : undefined}
+          flexBasis={isNotMobileScreen ? "42%" : undefined}
           marginTop={isNotMobileScreen ? undefined : "2rem"}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
         {isNotMobileScreen && <Box flexBasis="25%"></Box>}
       </Box>
     </Box>
